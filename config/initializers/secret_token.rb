65f13cf2a2464aaf71ae0c703be4a18dce50e240
Dmytrii Nagirniak <dnagir@gmail.com>
2011-12-13 13:42:38 +1100

    generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrubyEncoding::Application.config.secret_token = '768ca4439b9f41f347e205310a0c0269080fd514828a00632fb374141fbcb095cf75b599dbb2519366135d8120e0e272e77f48cd1266af73df5161df45a890fc'
